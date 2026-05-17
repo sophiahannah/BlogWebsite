59befa892c27db93091235f28abfb6d38df03f8c
sophiahannah <hannahsophiafelonia10@gmail.com>
2026-05-17 23:53:51 +0800

    Fixed floating footer

--- BlogWebsite/Controllers/BlogController.cs
+++ BlogWebsite/Controllers/BlogController.cs
@@ -14,6 +14,7 @@ namespace BlogWebsite.Controllers
             _context = context;
         }
 
+        // --- READ ---
         public IActionResult Index()
         {
             var posts = _context.Posts.OrderByDescending(p => p.CreatedAt).ToList();
@@ -25,6 +26,68 @@ namespace BlogWebsite.Controllers
             var post = _context.Posts.FirstOrDefault(p => p.Id == id);
             if (post == null) return NotFound();
             return View(post);
+        }
+
+        // --- CREATE ---
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+   
+        [HttpPost]
+        public IActionResult Create(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Posts.Add(post);
+                _context.SaveChanges();
+                return RedirectToAction("Index"); // Balik sa feed pagkatapos mag-save
+            }
+            return View(post);
+        }
+
+        // --- UPDATE (EDIT) ---
+  
+        public IActionResult Edit(int id)
+        {
+            var post = _context.Posts.Find(id);
+            if (post == null) return NotFound();
+            return View(post);
+        }
+
+
+        [HttpPost]
+        public IActionResult Edit(Post post)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Posts.Update(post);
+                _context.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(post);
+        }
+
+        // --- DELETE ---
+        public IActionResult Delete(int id)
+        {
+            var post = _context.Posts.Find(id);
+            if (post == null) return NotFound();
+            return View(post);
+        }
+
+       
+        [HttpPost, ActionName("Delete")]
+        public IActionResult DeleteConfirmed(int id)
+        {
+            var post = _context.Posts.Find(id);
+            if (post != null)
+            {
+                _context.Posts.Remove(post);
+                _context.SaveChanges();
+            }
+            return RedirectToAction("Index");
         }
     }
 }
